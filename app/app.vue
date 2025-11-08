c05c6ed83c42fb454cd673ed78d69834ecee6f42
Katsiaryna Vaitsiulevich <katsiaryna.vaitsiulevich@modsen-software.com>
2025-11-08 13:26:18 +0300

    feat: initial project

---
+++ app/app.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+// const router = useRouter()
+
+const nav = [
+  { label: 'Home', to: '/' },
+  { label: 'About', to: '/about' }
+]
+</script>
+
+<template>
+  <div>
+    <!-- <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+        </ul>
+      </nav>
+    </header> -->
+    <NuxtLayout>
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ app/components/AppHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>djjdjd</div>
+</template>
